bba833cabc896a26be030e36bb82623f0951b29d
PenluPogi <santossophia43@gmail.com>
2025-05-29 22:03:57 +0800

    try ulit

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -23,12 +23,12 @@ class employeecontroller extends Controller
     public function store(Request $request)
     {
     $request->validate([
-        'fname' => 'required|max:255|',
-        'lname' => 'required|max:255|',
-        'midname' => 'required|max:255|',
-        'age' => 'required|',
-        'address' => 'required|max:255|',
-        'zip' => 'required|',
+        'fname' => 'required|max:255|string',
+        'lname' => 'required|max:255|string',
+        'midname' => 'required|max:255|string',
+        'age' => 'required|integer',
+        'address' => 'required|max:255|string',
+        'zip' => 'required|integer',
         
     ]);
 
@@ -36,5 +36,32 @@ class employeecontroller extends Controller
     return view ('employee.create');
     }
 
-    
+    public function edit( int $id)
+    {
+        $employees = ::($id);
+        return view ('employee.edit');
+    }
+
+    public function update(Request $request, int $id) {
+        {
+            $request->validate([
+                'fname' => 'required|max:255|string',
+                'lname' => 'required|max:255|string',
+                'midname' => 'required|max:255|string',
+                'age' => 'required|integer',
+                'address' => 'required|max:255|string',
+                'zip' => 'required|integer',
+                
+            ]);
+        
+            employee::($id)->($request->all());
+            return redirect ()->back()->with('status','Employee Updated Successfully!');
+            }
+    }
+
+    public function (int $id){
+        $employees = employee::($id);
+        $employees->delete();
+        return redirect ()->back()->with('status','Employee Deleted');
+    }
 }
