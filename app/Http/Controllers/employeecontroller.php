c454e22ba5acdb2222383cc77b4f6ad819c71aa6
PenluPogi <santossophia43@gmail.com>
2025-05-29 21:37:13 +0800

    push again

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -36,32 +36,5 @@ class employeecontroller extends Controller
     return view ('employee.create');
     }
 
-    public function edit( int $id)
-    {
-        $employees = ::($id);
-        return view ('employee.edit');
-    }
-
-    public function update(Request $request, int $id) {
-        {
-            $request->validate([
-                'fname' => 'required|max:255|string',
-                'lname' => 'required|max:255|string',
-                'midname' => 'required|max:255|string',
-                'age' => 'required|integer',
-                'address' => 'required|max:255|string',
-                'zip' => 'required|integer',
-                
-            ]);
-        
-            employee::($id)->($request->all());
-            return redirect ()->back()->with('status','Employee Updated Successfully!');
-            }
-    }
-
-    public function (int $id){
-        $employees = employee::($id);
-        $employees->delete();
-        return redirect ()->back()->with('status','Employee Deleted');
-    }
+    
 }
